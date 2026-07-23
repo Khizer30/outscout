22148e910caf50ab7ab02b92c35493b424b9afee
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-07-23 20:03:17 +0500

    Added Media Module

--- apps/api/src/modules/media/media.module.ts
+++ apps/api/src/modules/media/media.module.ts
@@ -1,3 +1,4 @@
+import { JWTModule } from "@modules/jwt/jwt.module";
 import { MediaRepository } from "@modules/media/domain/media.repository";
 import { CloudinaryProvider } from "@modules/media/infrastructure/cloudinary.provider";
 import { MediaCloudinaryRepository } from "@modules/media/infrastructure/mediaCloudinary.repository";
@@ -6,6 +7,7 @@ import { MediaService } from "@modules/media/services/media.service";
 import { Module } from "@nestjs/common";
 
 @Module({
+  imports: [JWTModule],
   controllers: [MediaController],
   providers: [CloudinaryProvider, { provide: MediaRepository, useClass: MediaCloudinaryRepository }, MediaService],
   exports: [MediaService]
