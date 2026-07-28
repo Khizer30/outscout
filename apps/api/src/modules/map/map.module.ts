a69c6e3b8c7fa6d7c4ed34dc416d4247d7b6357c
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-07-28 16:49:10 +0500

    Bug Fix

--- apps/api/src/modules/map/map.module.ts
+++ apps/api/src/modules/map/map.module.ts
@@ -1,3 +1,4 @@
+import { JWTModule } from "@modules/jwt/jwt.module";
 import { MapPlaceRepository } from "@modules/map/domain/mapPlace.repository";
 import { MapGooglePlacesRepository } from "@modules/map/infrastructure/mapGooglePlaces.repository";
 import { MapController } from "@modules/map/presentation/map.controller";
@@ -5,6 +6,7 @@ import { MapService } from "@modules/map/services/map.service";
 import { Module } from "@nestjs/common";
 
 @Module({
+  imports: [JWTModule],
   controllers: [MapController],
   providers: [
     MapService,
