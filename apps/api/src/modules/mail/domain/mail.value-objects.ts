de622ca95fc78abb792da69904c3cabeb42e4884
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-08-07 18:38:01 +0500

    Updated Logo

--- apps/api/src/modules/mail/domain/mail.value-objects.ts
+++ apps/api/src/modules/mail/domain/mail.value-objects.ts
@@ -1,5 +1,5 @@
 export class OutscoutBranding {
   static readonly PRIMARY_COLOR = "#FFFFFF";
   static readonly SECONDARY_COLOR = "#38BDF8";
-  static readonly LOGO_URL = "https://res.cloudinary.com/dl0xs7uz9/image/upload/v1783928238/logo_dark_d6gadw.png";
+  static readonly LOGO_URL = "https://res.cloudinary.com/dl0xs7uz9/image/upload/v1786109563/logo_dark_tozgbd.png";
 }
