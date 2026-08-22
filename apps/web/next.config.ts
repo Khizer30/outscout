620c1cc9e9d97497bc52935d08dc9689db5773c4
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-08-22 23:39:05 +0500

    Load TurboRepo Environment Variables

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,4 +1,9 @@
+import path from "path";
+import { config } from "dotenv";
 import type { NextConfig } from "next";
+
+// Load TurboRepo Environment Variables
+config({ path: path.resolve(__dirname, "../../.env") });
 
 const nextConfig: NextConfig = {};
 
